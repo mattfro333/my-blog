c4c05a5c17295e978735ad5eafbe077d656e510f
mattfro333 <mattfro333@gmail.com>
2020-04-18 01:41:03 -0600

    First Commit for Build

---
+++ src/build/precache-manifest.eff4936a4bff3d8dec4f3e36d3e86bbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf64e0e5af6fab280c63eeed68738769",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52b14346153d771dc767",
+    "url": "/static/css/main.6f9a58af.chunk.css"
+  },
+  {
+    "revision": "f1425ab659480fcd05e0",
+    "url": "/static/js/2.fe7aa150.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.fe7aa150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52b14346153d771dc767",
+    "url": "/static/js/main.604871e5.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
